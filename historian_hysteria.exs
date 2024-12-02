37fbe9d6e08558b18248f5b27f03840e5ef640f1
Oscar Rodas <60824325+oscarbrodas@users.noreply.github.com>
2024-12-01 21:18:31 -0500

    Added solution for part 2

--- historian_hysteria.exs
+++ historian_hysteria.exs
@@ -1,5 +1,7 @@
 import Integer
 
+# Part 1
+
 # Read file
 {_error, file} = File.read("historian_hysteria_input.txt")
 
@@ -28,3 +30,20 @@ diffs = for i <- 0..999 do
 end
 
 IO.puts(Enum.sum(diffs))
+
+
+# Part 2
+leftFrequencies = Enum.frequencies(left)
+rightFrequencies = Enum.frequencies(right)
+leftMap = Enum.map(left, fn x -> {x , Map.get(rightFrequencies, x)} end)
+
+simlarities = for l <- Map.keys(leftFrequencies) do
+  sharedFreq = elem(List.keyfind(leftMap, l, 0), 1)
+  if sharedFreq != nil do
+    l * Map.get(leftFrequencies, l) * sharedFreq
+  else
+    0
+  end
+end
+
+IO.puts(Enum.sum(simlarities))
